b46d76cf26f2708de3eebf9e674c56451a8fd5e0
Yongjin <hi653@naver.com>
2024-09-17 18:03:12 +0900

    commit

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,64 @@
+import {useRef, useState} from "react";
+import ResultModal from "./ResultModal.jsx";
+
+export default function TimerChallenge({title, targetTime}) {
+    const timer = useRef();
+    const dialog = useRef();
+
+    const [timeRemaining, setTimeRemaining] = useState(
+        targetTime * 1000
+    );
+
+
+    // const [timerExpired, setTimerExpired] = useState(false);
+    // const [timerStarted, setTimerStarted] = useState(false);
+
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if(timeRemaining <= 0 ){
+        clearInterval(timer.current);
+        dialog.current.open();
+    }
+
+    function handleReset(){
+        setTimeRemaining(targetTime*1000);
+    }
+
+    function handleStart() {
+        timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+            // setTimerExpired(true);
+            // dialog.current.open(); // dialog의 내장요소
+        }, 10);
+    }
+
+    function handleStop(){
+        dialog.current.open();
+        clearTimeout(timer.current) // 포인터 : timer의 id를 input으로 필요
+    }
+
+    return (
+        <>
+            <ResultModal
+                ref ={dialog}
+                targetTime = {targetTime}
+                remainingTime={timeRemaining}
+                onReset = {handleReset}
+            />
+        <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+            <button onClick={timerIsActive ? handleStop : handleStart}>
+                {timerIsActive ? 'Stop' : 'Start'} Challenge
+            </button>
+        </p>
+            <p className={timerIsActive ? 'active' : undefined}>
+                {timerIsActive ? "Time is running..." : "Timer inactive"}
+            </p>
+    </section>
+        </>
+    );
+}
